e22dcabd461337cfaee86e322fa5921cb9c7dca1
inspirAZN <jcaluza@ucsd.edu>
2014-04-15 23:45:22 -0700

    going back to regular user profile

--- app.js
+++ app.js
@@ -37,10 +37,8 @@ app.get('/', index.view);
 // twitter routes
 app.get('/twit', twit.view);
 app.post('/twit/search', twit.search);
-// app.get('/twit/profile', twit.profile);
-app.get('/twit/profile', function (req, res) {
-	res.json(req.user.profile);
-});
+app.get('/twit/profile', twit.profile);
+
 
 app.get('/authn/twitter', auth.passport.authenticate('twitter'));
 // app.get('/twit/login', twit.login);
