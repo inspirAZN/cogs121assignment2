84e8bf4ad12a8843c488ee9d598a70f591aa001d
inspirAZN <jcaluza@ucsd.edu>
2014-04-18 14:32:14 -0700

    please

--- app.js
+++ app.js
@@ -74,7 +74,7 @@ app.get('/auth/facebook', auth.passport.authenticate('facebook-canvas', { scope:
 app.get('/auth/facebook/callback', 
   auth.passport.authenticate('facebook-canvas', { successRedirect: '/',
                                              failureRedirect: '/error' }));
-app.get('/auth/facebook/canvas', 
+app.post('/auth/facebook/canvas', 
   auth.passport.authenticate('facebook-canvas', { successRedirect: '/',
                                              failureRedirect: '/auth/facebook/canvas/autologin' }));
 app.get('/auth/facebook/canvas/autologin', function( req, res ){
