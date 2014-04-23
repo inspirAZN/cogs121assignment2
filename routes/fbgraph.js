fb1b6910795e935b80879c7551cfde620b0fc68c
inspirAZN <jcaluza@ucsd.edu>
2014-04-23 11:12:56 -0700

    whoops

--- routes/fbgraph.js
+++ routes/fbgraph.js
@@ -70,8 +70,8 @@ exports.graphAPI = function (req, res) {
 
 exports.getFriends = function(req, res) {
 	// var friendGraph = {};
-	// var query = "SELECT uid1, uid2 FROM friend WHERE uid1 in (SELECT uid2 FROM friend WHERE uid1=me())";
-	// 	query += " AND uid2 IN (SELECT uid2 FROM friend WHERE uid1=me())";
+	var query = "SELECT uid1, uid2 FROM friend WHERE uid1 in (SELECT uid2 FROM friend WHERE uid1=me())";
+		query += " AND uid2 IN (SELECT uid2 FROM friend WHERE uid1=me())";
 
 	auth.graph.fql(query, function(err, json) {
 	// auth.graph.get("/me/friends?fields=name,picture", function(err, json) {
